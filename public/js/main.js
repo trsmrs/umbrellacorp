faf756e6e41acbe68776ca6ddbd5abf1eaa99bab
Tiago <trsminfo@gmail.com>
2022-12-10 13:15:06 -0300

    finalizando o formulário de cadastro e prevenindo o envio preventDefault

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,11 @@
-console.log('OK, este script está funcionando!')
-
-const foo = el => setTimeout(()=>{
-            console.log('Script confirmado!')
-},2000)
+const sendCad = document.querySelector('#btn-send')
 
 
-foo()
+
+// PRevinir o envio Automático dos dados do formulário de cadastro.
+const formSend = ()=>{
+    sendCad.addEventListener('click',(e)=>{
+        e.preventDefault()
+    })
+}
+formSend()
