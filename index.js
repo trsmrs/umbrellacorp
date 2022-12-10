0734e2d7b9d40a131e9957e5732438e4f9c3cc8b
Tiago <trsminfo@gmail.com>
2022-12-10 13:03:48 -0300

    meuprojeto

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+// definindo o template engine
+app.set('view engine', 'EJS')
+
+
+// MVC - MODEL - VIEW - CONTROLLER
+
+
+// definindo os arquivos estáticos - USADO SOMENTE SE NÃO FOR USADO UM TEMPLATE ENGINE
+// app.use(express.static(path.join(__dirname, 'views')))
+
+
+// definindo os arquivos públicos
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+// ROTAS
+app.get('/',(req, res)=>{
+    res.render('index')
+})
+app.get('/contact',(req, res)=>{
+    res.render('contact')
+})
+
+//  404 NOT FOUND
+app.use((req, res)=>{
+    res.send('Página não encontrada')
+})
+
+
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 8080
+app.listen(port, () => console.log(`Server running on port ${port}`))
+
+
+
---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+console.log('OK, este script está funcionando!')
+
+const foo = el => setTimeout(()=>{
+            console.log('Script confirmado!')
+},2000)
+
+
+foo()
