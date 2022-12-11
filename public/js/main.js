e6e2d2d2e2c445173c13bd9319e68d20bc27d534
Tiago <trsminfo@gmail.com>
2022-12-11 03:08:23 -0300

    finalizando a parte do formulário

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,30 @@
 const sendCad = document.querySelector('#btn-send')
-
+const spanTip = document.querySelector('.span-tip')
 
 
 // PRevinir o envio Automático dos dados do formulário de cadastro.
 const formSend = ()=>{
     sendCad.addEventListener('click',(e)=>{
         e.preventDefault()
+       
+        setTimeout(function(){
+            spanTip.style.display = 'block'
+            spanTip.animate([
+                { transform: 'translateX(0px)'},
+                { transform: 'translateX(100px)'},
+                { transform: 'translateX(99px)'},
+                { transform: 'translateX(0px)'},
+            ],{
+                duration: 3700,
+                iterations: 1,
+                easing: 'ease-in-out'
+            }
+            )
+        },1000)
         
+        setTimeout(function(){
+            spanTip.style.display = 'none'
+        },4000)
     })
 }
 formSend()
